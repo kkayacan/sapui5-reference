668107d294a6db4933ce65038f33839db22f5058
Kerem Kayacan <kkayacan@gmail.com>
2019-02-19 16:58:10 +0300

    SAPUI5 Walkthrough app

---
+++ sapui5-walkthrough/webapp/controller/Detail.controller.js
@@ -0,0 +1,50 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/UIComponent"
+], function (Controller, History, MessageToast, JSONModel, UIComponent) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
+
+		onInit: function () {
+			var oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			this.getView().setModel(oViewModel, "view");
+			
+			var oRouter = UIComponent.getRouterFor(this);
+			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+		},
+
+		_onObjectMatched: function (oEvent) {
+			this.byId("rating").reset();
+			this.getView().bindElement({
+				path: decodeURIComponent("/" + oEvent.getParameter("arguments").invoicePath),
+				model: "invoice"
+			});
+		},
+
+		onNavBack: function () {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = UIComponent.getRouterFor(this);
+				oRouter.navTo("overview", {}, true);
+			}
+		},
+
+		onRatingChange: function (oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
+		}
+
+	});
+});
---
+++ sapui5-walkthrough/webapp/controller/HelloDialog.js
@@ -0,0 +1,47 @@
+sap.ui.define([
+	"sap/ui/base/ManagedObject",
+	"sap/ui/core/Fragment",
+	"sap/ui/core/syncStyleClass"
+], function (ManagedObject, Fragment, syncStyleClass) {
+	"use strict";
+
+	return ManagedObject.extend("sap.ui.demo.walkthrough.controller.HelloDialog", {
+
+		constructor : function (oView) {
+			this._oView = oView;
+		},
+
+		exit : function () {
+			delete this._oView;
+		},
+
+		open : function () {
+			var oView = this._oView;
+
+			// create dialog lazily
+			if (!oView.byId("helloDialog")) {
+				var oFragmentController = {
+					onCloseDialog : function () {
+						oView.byId("helloDialog").close();
+					}
+				};
+				// load asynchronous XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "sap.ui.demo.walkthrough.view.HelloDialog",
+					controller: oFragmentController
+				}).then(function (oDialog){
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					// forward compact/cozy style into dialog
+					syncStyleClass(oView.getController().getOwnerComponent().getContentDensityClass(), oView, oDialog);
+					oDialog.open();
+				});
+			} else {
+				oView.byId("helloDialog").open();
+			}
+		}
+
+	});
+
+});
