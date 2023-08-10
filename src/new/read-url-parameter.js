f974f0685621e647c5036f569612db70fff21886
Kerem Kayacan <kkayacan@gmail.com>
2023-08-10 14:03:48 +0300

    Read URL parameters

--- src/new/read-url-parameter.js
+++ src/new/read-url-parameter.js
@@ -1,1 +1,7 @@
+this._eventId = jQuery.sap.getUriParameters().get("eventid");
+this._pernr = jQuery.sap.getUriParameters().get("pernr");
+this._type = jQuery.sap.getUriParameters().get("type");
+
+// OR
+
 this._appType = window.location.href.substr(window.location.href.indexOf("fapp") + 5, 1);
